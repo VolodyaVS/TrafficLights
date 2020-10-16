8703d00637160aa12f9a4fdcb117e1b1ecde7dec
Volodya <volodyavolga@protonmail.ch>
2020-10-16 17:13:27 +0300

    Initial Commit

---
+++ TrafficLights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLights
+//
+//  Created by Vladimir Stepanchikov on 16.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Vladimir Stepanchikov on 16.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
